8b3f0af831ff3be1ce53605c0990fd9c5fbeeee0
arnhaa <arnehaagensen@gmail.com>
2022-10-07 13:39:49 +0200

    added ssm parameter

--- modules/iamroleonly/main.tf
+++ modules/iamroleonly/main.tf
@@ -36,3 +36,17 @@ data "aws_iam_policy_document" "allow_readiam" {
     resources = ["*"]
   }
 }
+
+resource "aws_ssm_parameter" "networkacc" {
+  name      = "/commonroles/networkaccd"
+  type      = "String"
+  value     = var.network_account_id
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "iamacc" {
+  name      = "commonroles/iamacc"
+  type      = "String"
+  value     = var.iam_account_id
+  overwrite = true
+}
--- modules/iamroleonly/variables.tf
+++ modules/iamroleonly/variables.tf
@@ -1,4 +1,10 @@
-variable "account_id" {
+variable "network_account_id" {
   description = "The networking account to allow"
   type = string
 }
+
+variable "iam_account_id" {
+  description = "The iam account to allow"
+  type = string
+}
+
